9e75b93226dae34aac4d926611f67e4a9615da4a
osx-23 <idatktl5@gmail.com>
2026-09-23 12:11:51 -0500

    refactor(programacion): add supervisor facade boundary

---
+++ src/app/features/programacion/state/programacion-supervisor.facade.ts
@@ -0,0 +1,99 @@
+import { inject, Injectable } from '@angular/core';
+import { ProgramacionApiService } from '../data-access/programacion-api.service';
+import {
+  AgenteProgramacionExcepcionRequest,
+  AsignarSecuenciaRequest,
+  GrupoLiderRequest,
+  GuardarOrdenSecuenciaRequest,
+  GuardarProgramacionRequest
+} from '../models/programacion.models';
+import {
+  GenerarProgramacionRequest
+} from '../data-access/programacion-api.service';
+
+@Injectable({ providedIn: 'root' })
+export class ProgramacionSupervisorFacade {
+  private readonly api = inject(ProgramacionApiService);
+
+  getPlazas() {
+    return this.api.getPlazas();
+  }
+
+  getAgentes(plazaId: number) {
+    return this.api.getAgentes(plazaId);
+  }
+
+  getControladores(plazaId: number) {
+    return this.api.getControladores(plazaId);
+  }
+
+  getTurnos(
+    plazaId: number,
+    anio: number,
+    mes: number
+  ) {
+    return this.api.getTurnos(
+      plazaId,
+      anio,
+      mes
+    );
+  }
+
+  guardarTurnos(
+    request: GuardarProgramacionRequest
+  ) {
+    return this.api.guardarTurnos(request);
+  }
+
+  generarPropuesta(
+    request: GenerarProgramacionRequest
+  ) {
+    return this.api.generarPropuesta(request);
+  }
+
+  getGrupos(plazaId: number) {
+    return this.api.getGrupos(plazaId);
+  }
+
+  asignarLider(
+    request: GrupoLiderRequest
+  ) {
+    return this.api.asignarLider(request);
+  }
+
+  getSecuencias(plazaId: number) {
+    return this.api.getSecuencias(plazaId);
+  }
+
+  asignarSecuencia(
+    request: AsignarSecuenciaRequest
+  ) {
+    return this.api.asignarSecuencia(request);
+  }
+
+  guardarOrdenSecuencia(
+    request: GuardarOrdenSecuenciaRequest
+  ) {
+    return this.api.guardarOrdenSecuencia(request);
+  }
+
+  getExcepciones(plazaId: number) {
+    return this.api.getExcepciones(plazaId);
+  }
+
+  guardarExcepcion(
+    request: AgenteProgramacionExcepcionRequest
+  ) {
+    return this.api.guardarExcepcion(request);
+  }
+
+  desactivarExcepcion(
+    trabajadorId: number,
+    plazaId: number
+  ) {
+    return this.api.desactivarExcepcion(
+      trabajadorId,
+      plazaId
+    );
+  }
+}
